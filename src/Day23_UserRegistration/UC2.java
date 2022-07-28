e5bacab8cff7ac8978cfa8e77477e75f5900d8c3
SwarnaHG <swarnahg18@gmail.com>
2022-07-28 23:06:28 +0530

    Email Validation

--- src/Day23_UserRegistration/UC2.java
+++ src/Day23_UserRegistration/UC2.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class UC2 {
     static Scanner input = new Scanner(System.in);
-    /*** UC-1 :- As a User need to enter a valid First Name. ***/
+
     public static void firstNameValidation() {
         System.out.print("Enter First Name :- ");
         String first_name = input.nextLine();
@@ -17,10 +17,10 @@ public class UC2 {
 
         Matcher matcher = pattern.matcher(first_name);
 
-        System.out.println("First name pattern matching :- " + matcher.matches());
+        System.out.println("First name pattern matched:- " + matcher.matches());
     }
 
-    /*** UC-2 :- As a User need to enter a valid Last Name. ***/
+
     public static void lastNameValidation() {
         System.out.print("\nEnter last Name :- ");
         String last_name = input.nextLine();
@@ -31,13 +31,13 @@ public class UC2 {
 
         Matcher matcher = pattern.matcher(last_name);
 
-        System.out.println("Last name pattern matching :- " + matcher.matches());
+        System.out.println("Last name pattern matched :- " + matcher.matches());
     }
 
-    /**** Main Method ****/
+
     public static void main(String[] args) {
-        /*** Displayed Welcome Message ***/
-        System.out.println("---------------WELCOME TO USER REGISTRATION PROBLEM---------------\n");
+
+        System.out.println("WELCOME TO USER REGISTRATION PROBLEM\n");
 
         firstNameValidation();
 
---
+++ src/Day23_UserRegistration/UC3.java
@@ -0,0 +1,2 @@
+package Day23_UserRegistration;public class UC3 {
+}
