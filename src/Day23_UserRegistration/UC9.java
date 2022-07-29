5f8aa041c4c16f3fc4230fc4c3a784f2f232bcb3
SwarnaHG <swarnahg18@gmail.com>
2022-07-29 13:58:30 +0530

    Should clear all email samples provided separately

--- src/Day23_UserRegistration/UC9.java
+++ src/Day23_UserRegistration/UC9.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UC8 {
+public class UC9 {
     static Scanner input = new Scanner(System.in);
 
-    /*** UC-1 :- As a User need to enter a valid First Name. ***/
     public static void firstNameValidation() {
-        System.out.print("Enter First Name :- ");
         System.out.print("\nEnter First Name :- ");
         String first_name = input.nextLine();
 
@@ -19,10 +17,9 @@ public class UC8 {
 
         Matcher matcher = pattern.matcher(first_name);
 
-        System.out.println("First name pattern matching :- " + matcher.matches());
+        System.out.println("First name pattern matched :- " + matcher.matches());
     }
 
-    /*** UC-2 :- As a User need to enter a valid Last Name. ***/
     public static void lastNameValidation() {
         System.out.print("\nEnter last Name :- ");
         String last_name = input.nextLine();
@@ -35,21 +32,16 @@ public class UC8 {
 
         System.out.println("Last name pattern matching :- " + matcher.matches());
     }
-
-    /*** UC-3:- As a User need to enter a valid email. ***/
     public static void emailValidation() {
         System.out.print("\nEnter E-Mail :- ");
         String email = input.nextLine();
-
         String emailRegex = "^[\\w+-]+(\\.[\\w+-]+)*@[\\w]+(\\.[\\w]+)?(?=(\\.[A-Za-z_]{2,3}$|\\.[a-zA-Z]{2,3}$)).*$";       //email validation using regular expressions.
-
+        //String emailRegex = "^[\\w+-]+(\\.[\\w+-]+)*@[\\w]+(\\.[\\w]+)?(?=(\\.[A-Za-z_]{2,3}$|\\.[a-zA-Z]{2,3}$)).*$";       //UC-9:- All email validations using regular expressions.
         Pattern pattern = Pattern.compile(emailRegex);
         Matcher matcher = pattern.matcher(email);
-
-        System.out.println("E-Mail pattern matching :- " + matcher.matches());
+        System.out.println("E-Mail pattern matched:- " + matcher.matches());
     }
 
-    /*** UC-4:- As a User need to follow pre defined Mobile Format. ***/
     public static void mobileNumberValidation() {
         System.out.print("\nEnter mobile number with country code :- ");
         String mobileNum = input.nextLine();
@@ -59,34 +51,29 @@ public class UC8 {
         Pattern pattern = Pattern.compile(mobileNumRegex);
         Matcher matcher = pattern.matcher(mobileNum);
 
-        System.out.println("Mobile number pattern matching :- " + matcher.matches());
+        System.out.println("Mobile number pattern matched :- " + matcher.matches());
     }
 
-    /*** UC-7:- As a User need to follow pre-defined Password rules. Rule-3:- Should have at least 1 numeric number in the password. ***/
-    /*** UC-8:- As a User need to follow pre-defined Password rules. Rule-4:- Has exactly 1 Special Character. ***/
     public static void passwordValidation() {
-        System.out.println("\nEnter a password with 8 minimum characters :- ");
         System.out.print("\nEnter a password with 8 minimum characters :- ");
         String password = input.nextLine();
 
-        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$";      //Rule-3:- Should have at least 1 numeric number in the password.
-        //String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)(?=.*[a-z]).{8,}$";      //Rule-4:-  Has exactly 1 Special Character.
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)(?=.*[a-z]).{8,}$";      //Rule-4:-  Has exactly 1 Special Character.
 
         Pattern pattern = Pattern.compile(passwordRegex);
 
         Matcher matcher = pattern.matcher(password);
 
-        System.out.println("Password Validation :- " + matcher.matches());
+        System.out.println("Password Validated :- " + matcher.matches());
     }
 
-
     public static void main(String[] args) {
-
-        System.out.println("WELCOME TO USER REGISTRATION PROBLEM\n");
+        System.out.println("WELCOME TO USER REGISTRATION PROBLEM");
         firstNameValidation();
         lastNameValidation();
         emailValidation();
         mobileNumberValidation();
         passwordValidation();
     }
+
 }
